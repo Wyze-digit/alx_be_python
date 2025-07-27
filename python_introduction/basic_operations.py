31d6c1188e7dc6ef4cb49c9ba54aa4e404e43390
Wisdom Emmanuel <ifiokbassey.e@gmail.com>
2025-07-27 16:24:01 +0200

    The calculation for sum, difference, product added

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -4,6 +4,21 @@
 principal = 1000       # $1000
 rate = 0.05            # 5% annual interest
 time = 3               # 3 years
+number1 = 10
+number2 = 5
+
+# calculate sum of number1 and number2
+sum = number1 + number2 # sum of the variables
+print('Addition of',number1,'and',number2, 'is ', sum)
+
+# calculate difference of number2 and number1
+difference = number1 - number2
+print('Subtraction of',number1,'and',number2, 'is ',difference)
+
+# Product of number1 and number2
+product = number1 * number2
+print('Multiplication of',number1,'and ',number2, 'is ',difference)
+
 
 # Calculate simple interest
 interest = principal * rate * time
