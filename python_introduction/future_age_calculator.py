6a218556df44e4ce62218059c91d91e544ebbfd3
Wisdom Emmanuel <ifiokbassey.e@gmail.com>
2025-07-28 10:19:32 +0200

    variable name corrected in the code

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,9 @@
+
+# Ask the user for their current age
+current_age = int(input("How old are you? "))
+
+# Calculate how old they will be in 2050
+age = current_age + 27
+
+# Output the result
+print(f"In 2050, you will be {age} years old.")
