10744ea74b386dbe2ca89d6e7771c4449cfbf0e6
Wisdom Emmanuel <ifiokbassey.e@gmail.com>
2025-07-28 09:50:31 +0200

    all codes to convert hours to seconds done.

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,9 @@
+# Converting hours into minutes calculator
+
+hours = 2
+
+# Convert hours to seconds
+seconds = hours * 60 * 60
+
+# Display the result
+print(f"{hours} hour(s) is equal to {seconds} seconds.")
