008372dcada41be76c92935ce3a42d4b0888c162
Wisdom Emmanuel <ifiokbassey.e@gmail.com>
2025-07-30 00:51:22 +0200

    codes have been written and tested ok

---
+++ match_case_calculator.py
@@ -0,0 +1,28 @@
+# A Program for Simple Calculator using Match Case
+
+# Call to get user input
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+# Ask user for desired operation
+operation = input("Choose any of this operations (+, -, *, /): ")
+
+# Perform calculation using match-case
+match operation:
+    case "+":
+        result = num1 + num2
+        print(f"The result of {num1} + {num2} is {result}")
+    case "-":
+        result = num1 - num2
+        print(f"The result of {num1} - {num2} is {result}")
+    case "*":
+        result = num1 * num2
+        print(f"The result of {num1} * {num2} is {result}")
+    case "/":
+        if num2 == 0:
+            print("Cannot divide by zero.")
+        else:
+            result = num1 / num2
+            print(f"The result of {num1} / {num2} is {result}")
+    case _:
+        print("Invalid operation selected.")
