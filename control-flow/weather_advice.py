9ae947acd64fa54870be505b06c1f262cd941961
Wisdom Emmanuel <ifiokbassey.e@gmail.com>
2025-07-30 00:13:28 +0200

    code refactoring, change all logical statements

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,9 +1,14 @@
 # Weather Advice program
 # Request for weather condition from user and take input
-weather = str(input("What's the weather like today? (sunny/rainy/cold):."), )
+weather = input("What's the weather like today? (sunny/rainy/cold):").lower()
 
 # check the input response and provide clothing recommendations
-if weather == 'sunny': print("Wear a t-shirt and sunglasses")
-elif weather == 'rainy': print("Dont forget your umbrella and a raincoat")
-elif weather == 'cold': print("Make sure to wear a warm coat and a Scarf")
-else: print('Sorry, I dont have recommendations for this weather!')
+
+if weather == "sunny":
+      print("Wear a t-shirt and sunglasses")
+elif weather == "rainy":
+      print("Don't forget your umbrella and a raincoat")
+elif weather == "cold":
+      print("Make sure to wear a warm coat and a scarf")
+else:
+      print("Sorry, I dont have recommendations for this weather")
