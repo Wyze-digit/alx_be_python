5776405ba85e787788aa6158794ba25a2c7bce02
Wisdom Emmanuel <ifiokbassey.e@gmail.com>
2025-07-29 23:35:46 +0200

    Corrections made to the comments

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -9,9 +9,9 @@ recommend_cold = 'Make sure to wear a warm coat and a Scarf'
 
 # check the input response and provide clothing recommendations
 if weather == 'sunny':
-      print({recommend_sunny})
+      print(f"Wear a t-shirt and sunglasses.")
 elif weather == 'rainy':
-      print(f"{recommend_rainy}")
+      print(f"Dont forget your umbrella and a raincoat")
 elif weather == 'cold':
-      print(f'{recommend_cold}')
+      print(f"Make sure to wear a warm coat and a Scarf")
 else: print(f'Sorry, I dont have recommendations for this weather!')
