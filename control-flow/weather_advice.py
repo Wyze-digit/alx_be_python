169a89eb15c9806ebe682fddde8fde4d3c0b9a89
Wisdom Emmanuel <ifiokbassey.e@gmail.com>
2025-07-29 23:48:16 +0200

    corrected the indentations

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -3,10 +3,7 @@
 weather = str(input("What's the weather like today? (sunny/rainy/cold):."), )
 
 # check the input response and provide clothing recommendations
-if weather == 'sunny':
-      print(f"Wear a t-shirt and sunglasses")
-elif weather == 'rainy':
-      print(f"Dont forget your umbrella and a raincoat")
-elif weather == 'cold':
-      print(f"Make sure to wear a warm coat and a Scarf")
-else: print(f'Sorry, I dont have recommendations for this weather!')
+if weather == 'sunny': print("Wear a t-shirt and sunglasses")
+elif weather == 'rainy': print("Dont forget your umbrella and a raincoat")
+elif weather == 'cold': print("Make sure to wear a warm coat and a Scarf")
+else: print('Sorry, I dont have recommendations for this weather!')
