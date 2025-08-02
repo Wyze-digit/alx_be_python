fff5a8c3cbc5a0a205a516c5248d0f6e9a50b436
Wisdom Emmanuel <ifiokbassey.e@gmail.com>
2025-08-02 20:11:04 +0200

    correct the input statement to read see

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,7 +1,7 @@
 # Multiplication operation. get user's input and perform mulplication of that input number from 1 to 10
 
 # Ask the user to input a number
-number = int(input("Enter a number to generate its multiplication table: "))
+number = int(input("Enter a number to see its multiplication table: "))
 
 # Use a for loop to iterate from 1 to 10
 for i in range(1, 11):
