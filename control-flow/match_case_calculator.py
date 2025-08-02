84637f9952048ac7a7b9e696eb9a0881715209a7
Wisdom Emmanuel <ifiokbassey.e@gmail.com>
2025-08-02 19:53:43 +0200

    corrected the print statement to show result is

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -12,18 +12,18 @@ operation = input("Choose the operation (+, -, *, /): ")
 match operation:
     case "+":
         result = num1 + num2
-        print(f"The result of {num1} + {num2} is {result}")
+        print(f"The result is {num1} + {num2}  {result}")
     case "-":
         result = num1 - num2
-        print(f"The result of {num1} - {num2} is {result}")
+        print(f"The result is {num1} - {num2}  {result}")
     case "*":
         result = num1 * num2
-        print(f"The result of {num1} * {num2} is {result}")
+        print(f"The result is {num1} * {num2}  {result}")
     case "/":
         if num2 == 0:
             print("Cannot divide by zero.")
         else:
             result = num1 / num2
-            print(f"The result of {num1} / {num2} is {result}")
+            print(f"The result is {num1} / {num2}  {result}")
     case _:
         print("Invalid operation selected.")
