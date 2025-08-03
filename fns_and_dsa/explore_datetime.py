32ed87ee01b31a35a7b2f574f58bf1ac087d96e2
Wisdom Emmanuel <ifiokbassey.e@gmail.com>
2025-08-03 08:33:42 +0200

    function imported, code building started

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,7 @@
+# Familiarize yourself with Python’s datetime module by writing a 
+# script that performs specified operations with dates and times.
+
+# import the date-time library from python standard library functions
+import datetime
+
+# Function to display the current date and time 
