70233f694a9b77135440f4cd9bb102f46d15cef9
Wisdom Emmanuel <ifiokbassey.e@gmail.com>
2025-08-03 21:50:26 +0200

    value error done to reflect please keyword

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -13,7 +13,7 @@ def safe_divide(numerator, denominator):
         return f"The result of the division is {result:.1f}"
 
     except ValueError:
-        return "Error: Enter numeric values only."
+        return "Error: Please enter numeric values only."
     except ZeroDivisionError:
         return "Error: Cannot divide by zero."
 
