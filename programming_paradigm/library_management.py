b6afb5afdab42f683c65c4d922ef3d61af7b31b4
Wisdom Emmanuel <ifiokbassey.e@gmail.com>
2025-08-03 23:40:07 +0200

    file created first

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,2 @@
+# Objective: Solidify understanding of basic OOP concepts in Python by implementing a system that tracks books in a library, 
+# focusing on classes, object instantiation, and method invocation.
