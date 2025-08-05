b0b43961482154f8a6f2874402a15cd2f7808f9a
Wisdom Emmanuel <ifiokbassey.e@gmail.com>
2025-08-05 11:06:54 +0200

    new codes added to main, library system file created with comments

---
+++ oop/library_system.py
@@ -0,0 +1,4 @@
+# Mastering Inheritance and Composition in Python
+# Objective: Deepen your understanding of inheritance and 
+# composition in Python by creating a system that models a library with different types of books.
+
